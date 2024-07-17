12cad5927f130884e45443a8ebe0051f5ba9c10e
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-17 14:52:48 +0530

    struct vs classes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Classes_Vs_Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Location a = new Location(20, 40);
+            Location b = a; //structs are value type, hence copy of a is equaled to b  whereas classes are reference types
+            a.x = 100;
+            
+
+            Console.WriteLine(b.x);
+        }
+    }
+
+    struct Location
+    {
+        public int x, y;
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+}
